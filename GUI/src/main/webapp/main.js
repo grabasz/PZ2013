f0a034ed7f8c93d71c87bc4e1cf0320ec446c827
Dariusz Chlipala <darius01@o2.pl>
2013-05-26 10:50:50 +0200

    Improved javascript chart

--- GUI/src/main/webapp/main.js
+++ GUI/src/main/webapp/main.js
@@ -5,13 +5,30 @@
 
 
 $('document').ready(function(){
-  //var data=[];
+  var data=[];
   var plot_container = $("#Plot");
   var limit=100;
-  var data = new Array(limit);
-  for(var i=0; i<limit; i++){
-     data[i]=[i, 0];//[i+1-this.plot_limit, 0];
-  }
+//  var data = new Array(limit);
+//  for(var i=0; i<limit; i++){
+//     data[i]=[i, 0];//[i+1-this.plot_limit, 0];
+//  }
+/**
+ * %a: weekday name (customizable)
+%b: month name (customizable)
+%d: day of month, zero-padded (01-31)
+%e: day of month, space-padded ( 1-31)
+%H: hours, 24-hour time, zero-padded (00-23)
+%I: hours, 12-hour time, zero-padded (01-12)
+%m: month, zero-padded (01-12)
+%M: minutes, zero-padded (00-59)
+%q: quarter (1-4)
+%S: seconds, zero-padded (00-59)
+%y: year (two digits)
+%Y: year (four digits)
+%p: am/pm
+%P: AM/PM (uppercase version of %p)
+%w: weekday as number (0-6, 0 being Sunday)
+ */
   var config = {
             yaxis: {
                 min: 0,
@@ -19,10 +36,21 @@ $('document').ready(function(){
             },
             xaxis: {
                 mode: "time",
-    timeformat: "%Y/%m/%d"
+                timeformat: "%M:%S"
             }};
   var plot=$.plot(plot_container, [data], config);
-   
+  
+  var makeSortedUniqeArray=function(arr){
+      
+    arr = arr.sort(function (a, b) { return a[0] - b[0]; });
+    var ret = [arr[0]];
+    for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+        if (arr[i-1][0] !== arr[i][0]) {
+            ret.push(arr[i]);
+        }
+    }
+    return ret;    
+  }
   
   setInterval(function(){
   $.ajax({
@@ -33,10 +61,17 @@ $('document').ready(function(){
       console.log(dt)
   },
   error: function(resp){
-      //console.log("data errror")
-      console.log(resp.responseText)
-      data=data.concat(JSON.parse(resp.responseText)).slice(1);
-    //  plot.de
+      new_values=JSON.parse(resp.responseText)
+      now =new Date();
+      
+      
+      
+      data=makeSortedUniqeArray(data.concat(new_values)
+      ).filter(function(elem, index, array){
+         return (now.valueOf()-120*1000<elem[0] );
+      });//.slice(1);
+      
+      
       plot=$.plot(plot_container, [data], config);
       //plot.setData([data]);
       //plot.draw();
