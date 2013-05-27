78b5397ae9c3395e383833d242e46f3420404943
sylwiaw <sylwnek@gmail.com>
2013-05-27 23:32:39 +0200

    Update ComplexMeasurement.java

--- ComplexMeasurement.java
+++ ComplexMeasurement.java
@@ -1,9 +1,25 @@
 package zespolowe;
 
-import java.util.LinkedList;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public class ComplexMeasurement extends Measurement{
-    ComplexMeasurement(int number,String operation){
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ *
+ * @author marcij
+ */
+public class ComplexMeasurement implements Runnable{
+
+    /**
+     * @param args the command line arguments
+     */
+     ComplexMeasurement(int number,String operation){
         this.number = number;
         this.operation = operation;
     }
@@ -12,30 +28,60 @@ public class ComplexMeasurement extends Measurement{
     String operation;
     LinkedList <Integer> data = new LinkedList();
     
+    boolean dataCollected = false;
+    protected int result = -1;
+    protected String operationTime;
+    int delay = 1000;
+    
+    public int getResult() {
+        return result;
+    }
+
+    public String getOperationTime() {       
+        return operationTime;
+    }
+    
+    
     public void collectData(){
+        Random rand = new Random();
         if (data.size()<number){
-            data.push(1);
+            data.push(rand.nextInt(100));
         }
         else {
+            dataCollected = true;
             data.poll();
-            data.push(1);
+            Date date = new Date();
+            SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss:SS");
+            operationTime = f.format(date);
+            data.push(rand.nextInt(100));
         }
     }
     
-    public int mean(){
+    public void mean(){
+        
         int i;
         int sum=0;
-        int mean;
         
         for (i=0;i<number;i++){
             sum += data.get(i);
         }
         
-        mean = sum/number;
-        return mean;
+        this.result = sum/number;
+        
+    }
+
+//w zależności od wybranej opcji wykonuje obliczenia i zwraca wynik
+    public void chooseOperation(){
+        if (dataCollected){
+            switch(operation){
+                case("mean") : this.mean();
+                case("maksimum") : this.maksimum();   
+                default : this.mean();    
+            }
+        }
     }
     
-    public int maksimum(){
+    public void maksimum(){
         
         int i;
         int max;
@@ -46,7 +92,38 @@ public class ComplexMeasurement extends Measurement{
             if (max<data.get(i)){max = data.get(i);}
         }
         
-        return max;
+        result = max;
     }
     
+    public void minimum(){
+        
+        int i;
+        int min;
+        
+        min = data.get(0);
+        
+        for (i=0;i<number;i++){
+            if (min<data.get(i)){min = data.get(i);}
+        }
+        
+        this.result = min;
+    }    
+    
+    public static void main(String[] args) {
+        ComplexMeasurement cm = new ComplexMeasurement(5,"mean");
+        cm.run();
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                this.collectData();
+                this.chooseOperation();
+                
+                Thread.sleep(delay);
+            }
+            catch(Exception e){}
+        }
+     }
 }
